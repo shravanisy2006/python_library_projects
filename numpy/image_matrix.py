2410f431a9629ef5e0e08e9824cc7420317a1386
shravanisy2006 <shravanisy2000@gmail.com>
2026-06-16 13:12:58 +0530

    Image placement edit

--- numpy/image_matrix.py
+++ numpy/image_matrix.py
@@ -7,38 +7,49 @@ image = np.array([
     [180, 220, 90]
 ])
 
-plt.figure(figsize=(4, 4))
-plt.subplot(1 , 2 ,1)
+plt.subplot(3 , 2 ,1)
 plt.imshow(image, cmap = 'gray')
 plt.title("Original Image")
 plt.axis('off')
 
 inverted = 255 - image
-plt.figure(figsize=(4, 4))
-plt.subplot(1 , 2 , 2)
+plt.subplot(3 , 2 , 2)
 plt.imshow(inverted , cmap= 'gray')
 plt.title("Inverted image")
 plt.axis('off')
 
 bright = image + 50
 bright = np.clip(bright, 0, 255)
-plt.figure(figsize=(4, 4))
-plt.subplot(1 , 2 , 1)
+plt.subplot(3 , 2 , 3)
 plt.imshow(bright , cmap= 'gray')
 plt.title("Brightened image")
 plt.axis('off')
 
 cropped = image[0:2, 0:2]
-plt.figure(figsize=(4, 4))
-plt.subplot(1 , 2 , 2)
+plt.subplot(3 , 2 , 4)
 plt.imshow(cropped , cmap= 'gray')
 plt.title("cropped image")
 plt.axis('off')
 
 rotated = np.rot90(image)
-plt.figure(figsize=(4, 4))
-plt.subplot(1 , 2 , 1)
+plt.subplot(3 , 1 , 3)
 plt.imshow(rotated , cmap= 'gray')
 plt.title("Rotated image")
 plt.axis('off')
+
+plt.tight_layout()
 plt.show()
+
+
+# fig, axes = plt.subplots(1, 5, figsize=(15, 4))
+
+# images = [image, inverted, bright, cropped, rotated]
+# titles = ["Original Image", "Inverted Image", "Brightened Image", "Cropped Image", "Rotated Image"]
+
+# for ax, img, title in zip(axes, images, titles):
+#     ax.imshow(img, cmap='gray')
+#     ax.set_title(title)
+#     ax.axis('off')
+
+# plt.tight_layout()
+# plt.show()
