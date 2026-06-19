b80c8a0447bd7f97f07bf0394c1912ffeeaa65d8
shravanisy2006 <shravanisy2000@gmail.com>
2026-06-19 10:03:06 +0530

    EDA for anime classification

--- pandas/anime.py
+++ pandas/anime.py
@@ -79,15 +79,69 @@ print(genre_scores.head(10))
 
 print("\n")
 
+print("=== Highest Rated TV Anime ===\n")
+highest_rated_tv = df[(df['Type'] == 'TV') & (df['Score'].astype(float).sort_values(ascending=False).head(1))]
+print(highest_rated_tv[['Name' , 'Score' , 'Genres']])
+
+print("\n")
+
+print("=== Highest Rated Movie Anime ===\n")
+highest_rated_movie = df[(df['Type'] == 'Movie') & (df['Score'].astype(float).sort_values(ascending=False).head(1))]
+print(highest_rated_movie[['Name' , 'Score' , 'Genres']])
+
+print("\n")
+
+print("=== Top 10 Anime by Members ===\n")
+top_members = df.sort_values(by='Members' , ascending = False).head(10)
+print(top_members[['Name' , 'Members' , 'Score']])
+
+print("\n")
+
+print("=== Top 10 Genres by count ===\n")
+top_genres = genre_series.value_counts().head(10)
+print(top_genres)
+
+print("\n")
+
 types.plot(kind='bar')
-plt.title("Anime Type Distrubution")
+plt.title("Anime Type Distribution")
+plt.tight_layout()
+plt.grid(axis = 'y')
 plt.xlabel("Type")
 plt.ylabel("Count")
 plt.savefig("anime_type_dist.png")
+plt.show()
 
-types.plot(kind='bar')
+top_genres.plot(kind='bar')
 plt.title("Top Genre Chart")
+plt.tight_layout()
+plt.grid(axis = 'y')
 plt.xlabel("Genre")
 plt.ylabel("Count")
-plt.savefig("anime_type_dist.png")
+plt.savefig("top_genres.png")
+plt.show()
+
+genre_scores.head(10).plot(kind='barh')
+
+plt.title("Top Genres by Average Score")
+plt.xlabel("Average Score")
+plt.ylabel("Genre")
+
+plt.grid(axis='x')
+plt.tight_layout()
+
+plt.savefig("genre_score_distribution.png")
+plt.show()
+
+top_anime_chart = (df.sort_values(by='Score',ascending=False).head(10))
+plt.barh(
+    top_anime_chart['Name'],
+    top_anime_chart['Score'].astype(float)
+)
+plt.title("Top 10 Rated Anime")
+plt.tight_layout()
+plt.grid(axis = 'y')
+plt.ylabel("Anime Name")
+plt.xlabel("Score")
+plt.savefig("top_10_rated.png")
 plt.show()
